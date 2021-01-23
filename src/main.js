056d80e860c04d71dcd89ce540bd06df10b3a880
Atropina <40372156+Atropina@users.noreply.github.com>
2021-01-23 15:01:32 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,13 +10,7 @@ Vue.config.productionTip = false
 
 
 var firebaseConfig = {
-  apiKey: "AIzaSyC7D5JKvKQVA2E39CDx0tq5ufkRSFGrQVY",
-  authDomain: "yag-inventario.firebaseapp.com",
-  projectId: "yag-inventario",
-  storageBucket: "yag-inventario.appspot.com",
-  messagingSenderId: "308242146113",
-  appId: "1:308242146113:web:4e8c662980e9b8dea9b7c0",
-  measurementId: "G-MN8Z20PEKC"
+  
 };
 
 firebase.initializeApp(firebaseConfig)
